c6ab0aee6c0c1dbe024b291e18dfe2138559a937
Nancy Mitchell <mitcheaven@gmail.com>
2018-12-29 21:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 function theBeatlesPlay(musicians, instruments) {
   let strings = []
   
-  for (let i = 0; i < musicians.length; i++)
+  for (let i = 0; i < musicians.length; i++) {
+    strings.push()
+  }
   
+  
+  return strings
 }
